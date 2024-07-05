5c0d7de4a81eaf04da5e44b2fc4ed282d69a7e2f
yahya <yahyaelmhadder7@gmail.com>
2024-07-05 16:23:20 +0100

    php modified

--- db_page.php
+++ db_page.php
@@ -13,20 +13,19 @@ if (!$con) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$prenom = mysqli_real_escape_string($con, $_POST["prenom"]);
-	$email = mysqli_real_escape_string($con, $_POST["email"]);
-	$phone = mysqli_real_escape_string($con, $_POST["phone"]);
-	$message = mysqli_real_escape_string($con, $_POST["message"]);
-	$nom = mysqli_real_escape_string($con, $_POST["nom"]);
+	$prenom = $_POST["prenom"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$message = $_POST["message"];
+	$nom = $_POST["nom"];
 
 	$stmt = $con->prepare("INSERT INTO formulaire (nom, prenom, email, telephone, message) VALUES (?, ?, ?, ?, ?)");
 	$stmt->bind_param("sssss", $nom, $prenom, $email, $phone, $message);
 
 	if ($stmt->execute()) {
 		echo "Formulaire envoyé avec succès";
-		header("Location: 24_contacts.html");
-         exit();
-
+		header("Location: Contact.html");
+		exit();
 	} else {
 		echo "Formulaire non envoyé: " . $stmt->error;
 	}
@@ -35,4 +34,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 mysqli_close($con);
-?>
